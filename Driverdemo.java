b84f3d0054d41915be5914dc0f1f5132f2b6d49e
Harshithagopidi <harshithareddygopidi3010@gmail.com>
2023-09-12 12:35:55 -0700

    my second commit

---
+++ Driverdemo.java
@@ -0,0 +1,22 @@
+package mavenproject.mavenproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Driverdemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in/");
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("తెలుగు")).click();
+		System.out.println("telugu language ");
+		driver.close();
+	}
+}

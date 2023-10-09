9304fca834399f38a1505f9e97a7270c52c4064e
Harshithagopidi <144645322+Harshithagopidi@users.noreply.github.com>
2023-10-09 19:20:10 +0530

    array pratice

---
+++ ArrayList/ArrayList01.java
@@ -0,0 +1,19 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayList01 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> abc = new ArrayList<String>();
+		//abc.add(null);
+		abc.add("Harshitha");
+		abc.add("Reddy");
+		abc.add("prabhas");
+		abc.add("uppalapati");
+		System.out.println(abc);
+
+	}
+
+}
---
+++ ArrayList/LinkedList01.java
@@ -0,0 +1,13 @@
+package ArrayList;
+
+import java.util.LinkedList;
+
+public class LinkedList01 {
+
+	public static void main(String[] args) {
+      LinkedList<String> xyz=new  LinkedList<String>();
+      
+
+	}
+
+}
